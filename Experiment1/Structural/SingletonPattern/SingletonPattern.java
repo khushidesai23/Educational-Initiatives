85f937205624480159ba132782a75ec014d1b9ec
Khushi Desai <110835870+khushidesai23@users.noreply.github.com>
2024-09-24 15:22:02 +0530

    final commit

---
+++ Experiment1/Structural/SingletonPattern/SingletonPattern.java
@@ -0,0 +1,16 @@
+package SingletonPattern;
+class DatabaseConnection {
+    private static DatabaseConnection instance = null;
+    private DatabaseConnection() {
+        // private constructor
+    }
+    public static DatabaseConnection getInstance() {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+    public void connect() {
+        System.out.println("Connected to database");
+    }
+}
